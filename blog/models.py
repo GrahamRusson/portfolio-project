a537fe55adfb7488a354e052dc5b7698f4ebee71
Graham Russon <grahamrusson@gmail.com>
2018-08-07 22:59:32 +0200

    finished

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,17 @@ class blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) >100:
+            return self.body[:100] + '...'
+        else:
+            return self.body
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
 
